bd3df12387e3e7d43e33459abe870b491da5edc0
Jon Packer <contact@jonpacker.com>
2015-06-09 11:33:47 +0200

    create cypher query from json object

--- seraph-snapshot.js
+++ seraph-snapshot.js
@@ -3,6 +3,7 @@ var fetchdb = [
   'OPTIONAL MATCH node-[rel]-x',
   'RETURN node, labels(node) as labels, collect(rel) as rels'
 ].join(' ');
+
 function dbToJSON(db, cb) {
   db.query(fetchdb, function(err, data) {
     if (err) return cb(err);
@@ -23,6 +24,47 @@ function dbToJSON(db, cb) {
     cb(null, normalized);
   });
 }
+
+function JSONtoCypher(data) {
+  var keys = Object.keys(data);
+  var nodes = keys.filter(function(key) {
+    return data[key].type == 'node';
+  });
+  var rels = keys.filter(function(key) {
+    return data[key].type == 'rel';
+  });
+  var keymap = {};
+  // can't use query params as we're exporting to a single string
+  function createParamString(obj) {
+    var kvs = Object.keys(obj).map(function(key) {
+      return key + ':' + JSON.stringify(obj[key]);
+    }).join(',');
+    return '{' + kvs + '}';
+  }
+  var uniqueNameIndex = 0;
+  function createUniqueName(type) {
+    return type + (uniqueNameIndex++);
+  }
+  var creates = [];
+  nodes.forEach(function(nodeKey) {
+    var node = data[nodeKey];
+    delete node.data.id;
+    var name = keymap[nodeKey] = createUniqueName('node');
+    var params = createParamString(node.data);
+    var labels = node.labels.map(function(l) { return ':`' + l + '`' }).join('');
+    creates.push('(' + name + labels + ' ' + params + ')');
+  });
+  rels.forEach(function(relKey) {
+    var rel = data[relKey];
+    var name = keymap[relKey] = createUniqueName('rel');
+    var params = createParamString(rel.data.properties);
+    var start = keymap["node_" + rel.data.start];
+    var finish = keymap["node_" + rel.data.end];
+    var type = '`' + rel.data.type + '`';
+    creates.push(start + '-[' + name + ':' + type + ' ' + params + ']->' + finish); 
+  });
+  return 'CREATE ' + creates.join(',');
+};
 
 module.exports = {
 
