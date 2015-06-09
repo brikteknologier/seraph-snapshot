f549b100032f4b183e623fe497e44ff33766ab9f
Jon Packer <contact@jonpacker.com>
2015-06-09 11:31:24 +0200

    fetchdb to json object function

--- seraph-snapshot.js
+++ seraph-snapshot.js
@@ -1,9 +1,27 @@
-function dbToJSON(db, callback) {
-  var fetchdb = [
-    'MATCH node',
-    'OPTIONAL MATCH node-[rel]-x',
-    'RETURN node, labels(node) as labels, collect(rel) as rels'
-  ].join(' ');
+var fetchdb = [
+  'MATCH node',
+  'OPTIONAL MATCH node-[rel]-x',
+  'RETURN node, labels(node) as labels, collect(rel) as rels'
+].join(' ');
+function dbToJSON(db, cb) {
+  db.query(fetchdb, function(err, data) {
+    if (err) return cb(err);
+    var normalized = {};
+    data.forEach(function(row) {
+      normalized['node_'+row.node.id] = {
+        type: 'node',
+        labels: row.labels,
+        data: row.node
+      };
+      row.rels.forEach(function(rel) {
+        normalized['rel_'+rel.id] = {
+          type: 'rel',
+          data: rel
+        };
+      });
+    });
+    cb(null, normalized);
+  });
 }
 
 module.exports = {
